5368db4712561c1ed546a5b6d9d49d96c602b99c
Peter Banh <peterbanhe.e@gmail.com>
2024-09-24 19:56:01 +0000

    Database setup

--- playground/polls/admin.py
+++ playground/polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question;
+ 
+#  Register your models here.
+
+# places the question model onto the admin page!
+# you can now preview all entries for the table graphically at localhost:8000/admin
+admin.site.register(Question)
--- playground/polls/models.py
+++ playground/polls/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Question(models.Model):
+    question_test = models.CharField(max_length=50)
+    pub_date = models.DateTimeField("date published")
+
+
+    def __str__(self):
+        return self.question_test
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) 
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
+
+# on the command line terminal: 
+# python manage.py migrate => creates the tables as designated in settings.py INSTALLED_APPS.
+# migrations are stored in disk 
+# makemigrations command is great for development since you can share these files with your fellow devs!
